4fdde619a9d37180d80e00d4ce16acf32ff32fb4
ashiqurras <ashiqurrussel2@gmail.com>
2022-01-14 20:24:47 +0100

    TypeScript function

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('test');
+let greet;
+greet = () => {
+    console.log("hello ");
+};
+greet();
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 6, 20);
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,18 @@
-console.log('test')
+let greet: Function;
+greet = () => {
+    console.log("hello ")
+}
+greet();
+
+const add = (a: number, b: number, c: number | string = 10): void => { //  default parameter put on last
+    console.log(a + b);
+    console.log(c)
+}
+
+add(5, 6, 20)
+
+const minus = (a: number, b: number) => {
+    return a + b;
+}
+
+let result = minus(10, 7)
