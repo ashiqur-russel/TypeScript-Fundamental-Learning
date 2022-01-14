98e2c01c1d38e1aa041288a48ea9e5c8d2907109
ashiqurras <ashiqurrussel2@gmail.com>
2022-01-14 18:28:48 +0100

    type Basics

---
+++ sandbox.js
@@ -0,0 +1,9 @@
+var character = 'luigi';
+var age = 30;
+var isBlackBelt = false;
+console.log(character);
+character = "Russel";
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
---
+++ sandbox.ts
@@ -0,0 +1,14 @@
+let character = 'luigi';
+let age = 30;
+let isBlackBelt = false;
+
+console.log(character);
+
+character = "Russel"
+
+const circ = (diameter: number) => {
+
+    return diameter * Math.PI;
+}
+
+console.log(circ(7.5))
