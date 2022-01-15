af383c8bb72355cf67fcae2f7f367465240b9fe2
ashiqurras <ashiqurrussel2@gmail.com>
2022-01-15 20:35:58 +0100

    Interface

--- public/app.js
+++ public/app.js
@@ -1,11 +1,34 @@
+const me = {
+    name: 'shaun',
+    age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+const greetPerson = (person) => {
+    console.log('Hello ', person.name);
+};
+greetPerson(me);
 import { Invoice } from './classes/invoice.js';
 const invOne = new Invoice('mario', 'work on website development', 400);
 const invTwo = new Invoice('Luigi', 'work as Backend development', 600);
-//console.log(invOne, invTwo)
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-//console.log(invoices)
 invoices.forEach(inv => {
     console.log(inv.client, inv.amount, inv.fromat());
+});
+const form = document.querySelector('.new-item-form');
+//inputs
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,61 @@
+//Interface 
+interface Isperson {
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number): number;
+}
+
+const me: Isperson = {
+    name: 'shaun',
+
+    age: 30,
+    speak(text: string): void {
+        console.log(text);
+    },
+    spend(amount: number): number {
+        console.log('I spent', amount);
+        return amount;
+    }
+}
+
+const greetPerson = (person: Isperson) => {
+    console.log('Hello ', person.name)
+}
+
+greetPerson(me)
 import { Invoice } from './classes/invoice.js'
 
 const invOne = new Invoice('mario', 'work on website development', 400);
 const invTwo = new Invoice('Luigi', 'work as Backend development', 600);
-
-//console.log(invOne, invTwo)
 
 
 let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 
-//console.log(invoices)
-
 invoices.forEach(inv => {
     console.log(inv.client, inv.amount, inv.fromat())
 })
 
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+//inputs
+
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+    console.log(
+        type.value,
+        tofrom.value,
+        details.value,
+        amount.valueAsNumber
+    )
+})
 
 
